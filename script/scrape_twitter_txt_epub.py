69b42bf7628a617e5b0dc860ef66f30da80dbfc7
Martin Moene <martin.moene@gmail.com>
2020-10-31 12:10:00 +0100

    Implement creation of epub

--- script/scrape_twitter_txt_epub.py
+++ script/scrape_twitter_txt_epub.py
@@ -12,6 +12,7 @@
 # Dependencies:
 # - npm, typescript, node
 # - twitter-archive-reader (TypeScript)
+# - Python
 #
 # Process:
 # 1. Read twitter archive, create epub/filename.txt with typescript/scrape_twitter_dm.ts
@@ -58,9 +59,17 @@ def to_date_only(time):
     """Convert date from '2019-05-08T08:27:07.472Z' to 'Wed 08 May 2019'"""
     return to_date(time)[:15]
 
+# Dictionary with CSS style for sender-name; see also create_css_participant_styles()
+# {'sender-name':'sender1', ...}
+css_name_dict = {}
+
+def to_css_class(name):
+    """Convert name"""
+    return css_name_dict[name] if name in css_name_dict else 'sender1'
+
 def to_name(name):
     """Convert name"""
-    return '<span class={name}>{name}</span> <span class=namedash>&ndash;</span> '.format(name=name)
+    return '<span class={css_class}>{name}</span> <span class=namedash>&ndash;</span> '.format(css_class=to_css_class(name), name=name)
 
 def to_heading(time):
     global last_year
@@ -97,23 +106,71 @@ def to_outname(folder, path, ext):
     """Convert filename.org to folder/filename.ext"""
     return os.path.join(folder, os.path.splitext(os.path.split(path)[1])[0] + ext)
 
-def convert_dm_text_epub(args, dm_path, dst_folder, include_tags, verbose):
-    convert_dm_text_markdown(args, dm_path, dst_folder, include_tags, verbose)
-    convert_dm_markdown_epub(args, dm_path, dst_folder, include_tags, verbose)
+def convert_dm_text_epub(args, dm_text_path, dst_folder, verbose):
+    """Create epub via markdown from messages collected in '{}' in folder '{}'."""
+    if verbose:
+        print('\n{}'.format(convert_dm_text_epub.__doc__.format(dm_text_path, dst_folder)))
 
-def convert_dm_text_markdown(args, dm_path, dst_folder, include_tags, verbose):
+    dm_markdown_path = to_outname(dst_folder, dm_text_path, '.md')
+    dm_epub_path     = to_outname(dst_folder, dm_text_path, '.epub')
+
+    create_css_participant_styles(args)
+    convert_dm_text_markdown(args, dm_text_path, dm_markdown_path, verbose)
+    convert_dm_markdown_epub(args, dm_markdown_path, dm_epub_path, verbose)
+
+    if verbose:
+        print('Done.')
+
+def create_css_participant_styles(args):
+    global css_name_dict
+    if args.css_participants:
+        for arg in args.css_participants.split(','):
+            key, value = arg.split(':')
+            css_name_dict[key] = value
+
+def convert_dm_text_markdown(args, dm_text_path, dm_markdown_path, verbose):
+    """- Create markdown '{}' from messages collected in '{}'."""
     # print( args )
-    import codecs
+    if verbose:
+        print(convert_dm_text_markdown.__doc__.format(dm_markdown_path, dm_text_path))
     # Pandoc reads and writes utf-8: "utf-8-sig"
-    with codecs.open(to_outname(dst_folder, dm_path, '.md'), "w", "utf-8", errors='surrogateescape') as out:
-        with codecs.open(dm_path, "r", "utf-8", errors='surrogateescape') as f:
+    import codecs
+    with codecs.open(dm_markdown_path, "w", "utf-8", errors='surrogateescape') as out:
+        with codecs.open(dm_text_path, "r", "utf-8", errors='surrogateescape') as f:
             for line in f:
                 time, name, text = line.split('\t')
                 out.write("{heading}<div class=entry>{name}{text}&ensp;{date}\n</div>\n\n".format(heading=to_heading(time), name=to_name(name), text=to_text(text), date=to_small_date(time)) )
 
-def convert_dm_markdown_epub(args, dm_path, dst_folder, include_tags, verbose):
-    # print( args )
-    pass
+def convert_dm_markdown_epub(args, dm_text_path, dm_epub_path, verbose):
+    """- Create epub '{}' from messages collected in '{}'."""
+    if verbose:
+        print(convert_dm_markdown_epub.__doc__.format(dm_epub_path, dm_text_path))
+
+    pandoc_tpl = '{pandoc} -tmarkdown-smart --self-contained --toc-depth={toc_depth} --epub-chapter-level={chapter_level} --metadata=author:"{author}" --metadata=title:"{title}" {date} {publisher} {rights} {cover} --css="{css}" --from="{fmt_from}" --to="{fmt_to}" {template} -o "{output}" "{input}"'
+
+    pandoc_cmd = pandoc_tpl.format(
+        pandoc='pandoc',
+        toc_depth=3,
+        chapter_level=1,
+        author=args.author,
+        title=args.title,
+        date='--metadata=date:"{}"'.format(args.date) if args.date else '',
+        rights='--metadata=rights:"{}"'.format(args.rights) if args.rights else '',
+        publisher='--metadata=publisher:"{}"'.format(args.publisher) if args.publisher else '',
+        cover='--epub-cover-image="{}"'.format(args.cover_image) if args.cover_image else '',
+        template='--template="{}"'.format(args.epub_template) if args.epub_template else '',
+        fmt_from=args.md_format,
+        fmt_to='epub',
+        css=args.css,
+        output=dm_epub_path,
+        input=dm_text_path
+    )
+    if verbose:
+        print('\nPandoc commandline:\n\n{}\n'.format(pandoc_cmd))
+
+    import shlex, subprocess
+    pandoc_cmd_args = shlex.split(pandoc_cmd)
+    subprocess.Popen(pandoc_cmd_args).wait()
 
 def main():
     """Convert direct messages from text in source folder via markdown to epub in destination folder."""
@@ -171,6 +228,12 @@ def main():
         help='file with css style sheet for epub')
 
     parser.add_argument(
+        '--css-participants',
+        metavar='styles',
+        type=str,
+        help='names with css styles, like name1:p1,name2:p2')
+
+    parser.add_argument(
         '--author',
         metavar='text',
         type=str,
@@ -189,6 +252,20 @@ def main():
         help='date or year range of e-book')
 
     parser.add_argument(
+        '--publisher',
+        metavar='text',
+        default=None,
+        type=str,
+        help='publisher of e-book')
+
+    parser.add_argument(
+        '--rights',
+        metavar='text',
+        default=None,
+        type=str,
+        help='publisher of e-book')
+
+    parser.add_argument(
         'filename',
         metavar='path',
         type=str,
@@ -200,9 +277,9 @@ def main():
     #print(args)
 
     if not args.dst_folder:
-        print("Option '--dst-folder=folder' is required. See '{} -h' for more help.".format(sys.argv[0]))
+        print("\nOption '--dst-folder=folder' is required. See '{} -h' for more help.\n".format(sys.argv[0]))
     else:
-        convert_dm_text_epub(args, args.filename[0], args.dst_folder, args.include_tags, args.verbose)
+        convert_dm_text_epub(args, args.filename[0], args.dst_folder, args.verbose)
 
 if __name__ == '__main__':
     main()
